5ead0376fe4c8007e43fd1a459b5e80093a6233c
nverno <noah.v.peart@gmail.com>
2021-05-09 16:16:27 -0400

    use cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+const cors = require('cors');
+// const path = require('path');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: __dirname + '/.env '});
@@ -8,21 +9,22 @@ dotenv.config({ path: __dirname + '/.env '});
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin,X-Requested-With,Content-Type,Accept,application/json'
+//   );
+//   res.header(
+//     'Access-Control-Allow-Methods', 'OPTIONS,POST,GET'
+//   );
+//   next();
+// });
 
 // Routes
 const xpath = require('./routes/api/xpath');
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin,X-Requested-With,Content-Type,Accept,application/json'
-  );
-  res.header(
-    'Access-Control-Allow-Methods', 'OPTIONS,POST,GET'
-  );
-  next();
-});
 app.use('/api/xpath', xpath);
 
 const port = process.env.PORT || 5000;
